202695ac2367774414a996c355286602df0fc8ab
shahlasamadli <shahlasamadli@gmail.com>
2020-02-21 22:31:50 +0400

    part 3

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+var hbar=document.querySelector(".hidden-bar")
+var menu=document.querySelector(".fmenu")
+hbar.onclick=function(){
+    menu.classList.toggle("active")
+}
+
+
+var fli=document.querySelector(".fli")
+var phmenu=document.querySelector(".phonemenu")
+fli.onclick=function(){
+    phmenu.classList.toggle("active")
+}
